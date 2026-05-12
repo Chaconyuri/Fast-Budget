ba510a4c543a1ee201f5e9f810f9d25c8349e1b3
joao4v <jbnwk1@gmail.com>
2026-05-12 19:58:35 -0300

    Delete antigo app directory

--- app/db/base.py
+++
@@ -1,5 +0,0 @@
-from app.db.session import Base
-from app.models.item import Item
-from app.models.quote import Quote, QuoteItem, QuoteService
-from app.models.service import Service
-from app.models.user import User
